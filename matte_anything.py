7623e5d30a135aa45c6cbca68b2ff2734d36c37a
Karol <karol.kulawiec@spectre.solutions>
2024-12-16 13:42:41 +0100

    create public link

--- matte_anything.py
+++ matte_anything.py
@@ -521,4 +521,4 @@ if __name__ == "__main__":
             with gr.Column():
                 background_image = gr.State(value=None)
 
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
